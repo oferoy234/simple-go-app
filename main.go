372c903ebc14ef057c01f154ae88c66592145de8
oferoy <royo@mail.tau.ac.il>
2025-05-02 18:32:41 +0300

    second

--- main.go
+++ main.go
@@ -12,6 +12,9 @@ func main() {
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "OK")
 	})
+	http.HandleFunc("/good", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "great")
+	})
 
 	fmt.Println("Server running on port 8080")
 	http.ListenAndServe("0.0.0.0:8080", nil)
